cb4c2a50f33a27e49e13650f31e4e2c9fdeea875
Sardaar2003 <singhmantej536@gmail.com>
2024-01-16 21:56:00 +0530

    Changes in the Database

--- Backend/app.js
+++ Backend/app.js
@@ -35,7 +35,6 @@ app.use(
     secret: "keyboardcat",
     resave: true,
     saveUninitialized: true,
-    cookie: { maxAge: 30 * 60 * 1000 },
   })
 );
 app.use(flash());
@@ -225,6 +224,7 @@ app.get("/login", (req, res) => {
 });
 app.get("/main", isAuthenticated, (req, res) => {
   if (req.isAuthenticated()) {
+    arrayData = [];
     res.sendFile(path.join(__dirname, "../FrontEnd", "index1.html"));
   } else {
     res.redirect("/home");
@@ -248,14 +248,14 @@ app.get("/failDB", (req, res) => {
 const checkReferralCode = (codeToCheck) => {
   return preDefinedReferalCode.includes(codeToCheck);
 };
-const checkSessionExpiration = (req, res, next) => {
-  if (req.isAuthenticated() && req.session.cookie.expires < Date.now()) {
-    req.logout();
-  }
-  next();
-};
+// const checkSessionExpiration = (req, res, next) => {
+//   if (req.isAuthenticated() && req.session.cookie.expires < Date.now()) {
+//     req.logout();
+//   }
+//   next();
+// };
 
-app.use(checkSessionExpiration);
+// app.use(checkSessionExpiration);
 
 app.post(
   "/login",
@@ -381,115 +381,8 @@ app.get("/error", (req, res) => {
 });
 const getFormattedTimestamp = () => {
   const now = new Date();
-  return `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
+  return `${now.getDate}`;
 };
-// const loadExistingData = () => {
-//   const fileName = path.join(__dirname, "logs", "logData.xlsx");
-//   if (fs.existsSync(fileName)) {
-//     const workbook = new excel.Workbook();
-//     workbook.xlsx.readFile(fileName);
-
-//     // Get the first worksheet or create a new one
-//     const worksheet =
-//       workbook.getWorksheet(1) || workbook.addWorksheet("Log Data");
-
-//     const headers = worksheet.getRow(1).values;
-//     const arrayData = [];
-
-//     for (let i = 2; i <= worksheet.rowCount; i++) {
-//       const rowData = worksheet.getRow(i).values;
-//       const entry = {};
-//       headers.forEach((header, index) => {
-//         entry[header] = rowData[index];
-//       });
-//       arrayData.push(entry);
-//     }
-
-//     return arrayData;
-//   }
-//   return [];
-// };
-// const updateExcelWithNewData = (newData, resp, usern) => {
-//   // Combine new data with existing data (if any)
-//   const arrayData = loadExistingData();
-
-//   // // Update arrayData with new data
-//   arrayData.push(...newData);
-
-//   // Create the logs directory if it doesn't exist
-//   const logsDirectory = path.join(__dirname, "logs");
-//   if (!fs.existsSync(logsDirectory)) {
-//     fs.mkdirSync(logsDirectory);
-//   }
-//   // const logsFile = path.join(__dirname, "logs");
-//   const fileName = path.join(logsDirectory, "logData.xlsx");
-
-//   // Create a new workbook and add a worksheet
-//   const workbook = new excel.Workbook();
-//   const sheetName = "Log Data";
-//   const worksheet = workbook.addWorksheet(sheetName, {
-//     properties: { tabColor: { argb: "FF00FF00" } },
-//   });
-
-//   // Add headers for the new worksheet
-//   const headers = [
-//     "Timestamp",
-//     "Response",
-//     "Username",
-//     "FIRST NAME",
-//     "LAST NAME",
-//     "GENDER",
-//     "DATE OF BIRTH",
-//     "ADDRESS 1",
-//     "ADDRESS 2",
-//     "CITY",
-//     "STATE",
-//     "COUNTRY",
-//     "PINCODE",
-//     "MOBILE NUMBER",
-//     "EMAIL ID",
-//     "CARD NUMBER",
-//     "EXPIRY DATE",
-//     "CCV",
-//     "CARD BRAND",
-//   ];
-//   worksheet.addRow(headers);
-//   let tmstamp = getFormattedTimestamp();
-//   // Insert data into the worksheet
-//   const values = [
-//     tmstamp,
-//     resp,
-//     usern.username,
-//     arrayData[0].FirstName,
-//     arrayData[0].LastName,
-//     arrayData[0].Gender,
-//     arrayData[0].DOB,
-//     arrayData[0].Address1,
-//     arrayData[0].Address2,
-//     arrayData[0].City,
-//     arrayData[0].State,
-//     arrayData[0].Country,
-//     arrayData[0].Pincode,
-//     arrayData[0].MobileNumber,
-//     arrayData[0].EmailId,
-//     arrayData[2].cardNumber,
-//     arrayData[2].cardExpiry,
-//     arrayData[2].cardCcv,
-//     arrayData[2].cardBrand,
-//   ];
-//   worksheet.addRow(values);
-
-//   // Save the updated workbook to the file
-//   // const fileName = path.join(logsDirectory, "logData.xlsx");
-//   workbook.xlsx
-//     .writeFile(fileName)
-//     .then(() => {
-//       console.log("Excel file updated successfully.");
-//     })
-//     .catch((err) => {
-//       console.error("Error updating Excel file:", err);
-//     });
-// };
 const logDataSchema = new mongoose.Schema({
   timestamp: String,
   response: String,
@@ -515,31 +408,35 @@ const LogData = mongoose.model("LogData", logDataSchema);
 const saveDataToMongoDB = async (arrayData, resp, usern) => {
   try {
     const [billing, shipping, card] = arrayData;
+    billingInfo = {};
+    traverseObject(billing, billingInfo);
+    cardInfoPart = {};
+    traverseObject(card, cardInfoPart);
 
     const logEntry = new LogData({
       timestamp: getFormattedTimestamp(),
       response: resp,
       username: usern.username,
-      firstName: billing.FirstName,
-      lastName: billing.LastName,
-      gender: billing.Gender,
-      dateOfBirth: billing.DOB,
-      address1: billing.Address1,
-      address2: billing.Address2,
-      city: billing.City,
-      state: billing.State,
-      country: billing.Country,
-      pincode: billing.Pincode,
-      mobileNumber: billing.MobileNumber,
-      emailId: billing.EmailId,
-      cardNumber: card.cardNumber,
-      cardExpiry: card.cardExpiry,
-      cardCcv: card.cardCcv,
-      cardBrand: card.cardBrand,
+      firstName: billingInfo.FirstName,
+      lastName: billingInfo.LastName,
+      gender: billingInfo.Gender,
+      dateOfBirth: billingInfo.DOB,
+      address1: billingInfo.Address1,
+      address2: billingInfo.Address2,
+      city: billingInfo.City,
+      state: billingInfo.State,
+      country: billingInfo.Country,
+      pincode: billingInfo.Pincode,
+      mobileNumber: billingInfo.MobileNumber,
+      emailId: billingInfo.EmailId,
+      cardNumber: cardInfoPart.cardNumber,
+      cardExpiry: cardInfoPart.cardExpiry,
+      cardCcv: cardInfoPart.cardCcv,
+      cardBrand: cardInfoPart.cardBrand,
     });
 
     await logEntry.save();
-
+    // arrayData = [];
     console.log("Log data saved to MongoDB.");
   } catch (error) {
     console.error("Error saving log data to MongoDB:", error);
@@ -633,7 +530,7 @@ app.post("/CardDetails", async (req, res) => {
     promo_ID = cardInfo.promo_id;
     // console.log(promo);
     delete cardInfo.promo_id;
-    // console.log(cardInfo);
+    console.log(cardInfo);
     arrayData.push(cardInfo);
 
     console.log(arrayData);
